7e28581ddf6ce1fa5965b3f397e551816786fba5
RyndyukDanila <ryndyukdanila@gmail.com>
2024-03-04 16:06:00 +0600

    Removed hard-coded slivers in CustomScrollView

--- lib/src/features/menu/view/menu_screen.dart
+++ lib/src/features/menu/view/menu_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_course/src/features/menu/models/category.dart';
 import 'package:flutter_course/src/features/menu/models/drink.dart';
 import 'package:flutter_course/src/features/menu/view/widgets/categories_list.dart';
@@ -20,46 +19,23 @@ class MenuScreen extends StatelessWidget {
             backgroundColor: Theme.of(context).scaffoldBackgroundColor,
             flexibleSpace: const CategoriesList(),
           ),
-          _buildCategoryTitle(
-            categoriesList[0].key,
-            AppLocalizations.of(context)!.black_coffee,
-            Theme.of(context).textTheme.titleLarge!,
-          ),
-          SliverPadding(
-            padding:
-                const EdgeInsets.only(bottom: 16.0, left: 16.0, right: 16.0),
-            sliver: DrinksGrid(drinks: allDrinks),
-          ),
-          _buildCategoryTitle(
-            categoriesList[1].key,
-            AppLocalizations.of(context)!.milk_coffee,
-            Theme.of(context).textTheme.titleLarge!,
-          ),
-          SliverPadding(
-            padding:
-                const EdgeInsets.only(bottom: 16.0, left: 16.0, right: 16.0),
-            sliver: DrinksGrid(drinks: allDrinks),
-          ),
-          _buildCategoryTitle(
-            categoriesList[2].key,
-            AppLocalizations.of(context)!.tea,
-            Theme.of(context).textTheme.titleLarge!,
-          ),
-          SliverPadding(
-            padding:
-                const EdgeInsets.only(bottom: 16.0, left: 16.0, right: 16.0),
-            sliver: DrinksGrid(drinks: allDrinks),
-          ),
-          _buildCategoryTitle(
-            categoriesList[3].key,
-            AppLocalizations.of(context)!.signature_drinks,
-            Theme.of(context).textTheme.titleLarge!,
-          ),
-          SliverPadding(
-            padding:
-                const EdgeInsets.only(bottom: 16.0, left: 16.0, right: 16.0),
-            sliver: DrinksGrid(drinks: allDrinks),
-          ),
+          ...categoriesList.map((category) {
+            return SliverMainAxisGroup(
+              slivers: [
+                _buildCategoryTitle(
+                  category.key,
+                  category.name,
+                  // AppLocalizations.of(context)!.signature_drinks,
+                  Theme.of(context).textTheme.titleLarge!,
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.only(
+                      bottom: 16.0, left: 16.0, right: 16.0),
+                  sliver: DrinksGrid(drinks: allDrinks),
+                ),
+              ],
+            );
+          }),
         ],
       ),
     );
